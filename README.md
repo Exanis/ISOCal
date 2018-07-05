49c360496a78074d54e17979683aa8eba3245fce
Yann Piquet <zeffy.zaephyr@gmail.com>
2018-07-05 19:37:22 +0200

    Add script

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ISOCal
 Just a simple tool to sync office 365 calendar with Google Calendar in order to be able to use google home
+
+## Warning
+This script was made for my own usage. It may or may not fit your need.
+
+## How to use
+- Pre-requisite : have python3 and pip installed
+- Fill the `__main__.py` first lines with your own email / password for O365 (lines 6 and 7)
+- Also set your timezone if it's not the same as mine
+- Go to [this url](https://developers.google.com/calendar/quickstart/python) and follow the step 1 to get a `credentials.json` file
+- Put it in the same directory that `__main__.py` and name it `client_secret.json`
+- Install the requirements : `pip install -r requirements.txt`
+- Run the script : `python __main__.py`
+    - If you are running the script for the first time, a browser window will open to auth your google account with the app. Just hit accept :)
+
+If you need to keep your calendars in sync, just add a cron task to run this script every so often.
---
+++ __main__.py
@@ -0,0 +1,130 @@
+"""
+ISOCal
+Tool to sync events from office365 to google calendar
+"""
+import datetime
+import time
+from httplib2 import Http
+from apiclient.discovery import build
+from oauth2client import file, client, tools
+from O365 import Schedule
+
+
+O365_LOGIN = ''
+O365_PASSWORD = ''
+TIMEZONE = 'Europe/Paris'
+
+
+def gcal_auth():
+    """
+    Log to google api
+    """
+    scopes = 'https://www.googleapis.com/auth/calendar'
+    store = file.Storage('credentials.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('client_secret.json', scopes)
+        creds = tools.run_flow(flow, store)
+    service = build('calendar', 'v3', http=creds.authorize(Http()))
+    return service
+
+
+def gcal_get_events(service):
+    """
+    Retrieve events from Google Calendar to compare with o365
+    """
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    request = service.events().list(calendarId='primary', timeMin=now).execute()
+    return request.get('items', [])
+
+
+def o365_auth(username, password):
+    """
+    Log to O365 API
+    """
+    service = Schedule((username, password))
+    service.getCalendars()
+    return service
+
+
+def o365_get_events(service):
+    """
+    Retrieve all event in all calendars
+    """
+    events = []
+    for cal in service.calendars:
+        cal.getEvents()
+        events = events + [e for e in cal.events]
+    return events
+
+
+def get_google_date(event_date):
+    """
+    Retrieve the date from a google event
+    (google may store the data in two different place depending of the format)
+    """
+    if 'dateTime' in event_date:
+        return time.strptime(event_date['dateTime'].replace(':', ''), "%Y-%m-%dT%H%M%S%z")
+    return time.strptime(event_date['date'], '%Y-%m-%d')
+
+
+def is_same_event(g_event, o_event):
+    """
+    Return true if two events are the same
+    """
+    return g_event['summary'] == o_event.getSubject() \
+        and o_event.getStart() == get_google_date(g_event['start']) \
+        and o_event.getEnd() == get_google_date(g_event['end']) \
+
+
+def event_exist(g_events, event):
+    """
+    Return True if the given event (o365) already exist in google list
+    """
+    search_list = [
+        e for e in g_events if is_same_event(e, event)
+    ]
+    return bool(search_list)
+
+
+def filter_duplicate_events(g_events, o_events):
+    """
+    Remove already existing event from the list
+    """
+    filtered = [e for e in o_events if not event_exist(g_events, e)]
+    return filtered
+
+
+def add_events(g_service, events):
+    """
+    Add all events in list to google calendar
+    """
+    for event in events:
+        start = event.getStart()
+        end = event.getEnd()
+        request = g_service.events().insert(
+            calendarId="primary",
+            body={
+                "summary": event.getSubject(),
+                "description": event.getBody(),
+                "location": event.getLocation(),
+                "start": {
+                    "dateTime": time.strftime("%Y-%m-%dT%H:%M:%S", start),
+                    "timeZone": TIMEZONE
+                },
+                "end": {
+                    "dateTime": time.strftime("%Y-%m-%dT%H:%M:%S", end),
+                    "timeZone": TIMEZONE
+                }
+            }
+        )
+        request.execute()
+
+
+if __name__ == "__main__":
+    G_SERVICE = gcal_auth()
+    G_EVENTS = gcal_get_events(G_SERVICE)
+    O_SERVICE = o365_auth(O365_LOGIN, O365_PASSWORD)
+    O_EVENTS = o365_get_events(O_SERVICE)
+    NEW_EVENTS = filter_duplicate_events(G_EVENTS, O_EVENTS)
+    add_events(G_SERVICE, NEW_EVENTS)
